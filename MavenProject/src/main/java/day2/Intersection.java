eea0cfd95cf2bd1cde88200489ec92eac958b7bb
Nirmal021197 <nirmalpalanimudali@gmail.com>
2022-04-21 20:00:58 +0530

    test

---
+++ MavenProject/src/main/java/day2/Intersection.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class Intersection {
+
+	public static void main(String[] args) {
+	int [] a= {3,1,4,6,7};
+	int [] b= {1,2,8,9,5};
+	for(int i=0;i<a.length;i++) {
+		for(int j=0;j<b.length;j++) {
+			
+		if(a[i]==b[j])
+		{
+		System.out.println(a[i]);
+	
+		}
+			
+	}
+
+	}
+	}
+}
+
+
+
